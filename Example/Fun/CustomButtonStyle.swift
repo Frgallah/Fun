0e784d8925a8c9ed49e86084ab3a21158903b589
frgallah <frgallah@outlook.com>
2017-04-26 19:47:08 -0400

    Add classes to the Example

---
+++ Example/Fun/CustomButtonStyle.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButtonStyle.swift
+//  Fun
+//
+//  Created by Home on 4/26/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CustomButtonStyle: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
